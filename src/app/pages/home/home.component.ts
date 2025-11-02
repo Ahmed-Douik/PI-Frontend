9232ab74acbb547916a7d3eea562a0cb25ab8a73
Ahmed-Douik <a.douik456@gmail.com>
2025-11-02 23:10:16 +0100

    landing page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,27 +1,11 @@
-import { AfterViewInit, Component } from '@angular/core';
-import * as L from 'leaflet';
+import {Component } from '@angular/core';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements AfterViewInit {
-  private map!: L.Map;
+export class HomeComponent{
 
-  ngAfterViewInit(): void {
-    this.map = L.map('map').setView([51.505, -0.09], 13);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(this.map);
-
-    // Fix icons (Angular ne trouve pas les images sinon)
-    const iconDefault = L.icon({
-      iconUrl: 'assets/marker-icon.png',
-      shadowUrl: 'assets/marker-shadow.png',
-    });
-    L.Marker.prototype.options.icon = iconDefault;
-  }
 
 }
